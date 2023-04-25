e708f20687edd1310cff4bc299fb29e9e25f4a09
Łukasz Sypniewski <l.sypniewski@gmail.com>
2023-04-25 23:13:35 +0200

    Addd a controller with root path

---
+++ hellocontainers/Controllers/AnotherController.cs
@@ -0,0 +1,16 @@
+﻿using hellocontainers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hellocontainers.Controllers
+{
+    [ApiController]
+    public class AnotherController : ControllerBase
+    {
+        [HttpGet("/")]
+        public ActionResult<HelloResponse> GetResult(string? echo)
+        {
+            return new HelloResponse(Message, YouSaid: echo ?? "Nothing");
+        }
+        private string Message => Environment.GetEnvironmentVariable("HELLOCONTAINERS_MESSAGE") ?? "No Message Set!";
+    }
+}
